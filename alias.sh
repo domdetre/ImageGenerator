e9754a49e10a2f2b419e9fe97ae3564bec845e6d
Detre Dominus <dominusdetre@gmail.com>
2020-10-01 14:41:41 +0200

    Updating docker php cli

--- alias.sh
+++ alias.sh
@@ -1,2 +1,4 @@
 alias dc='docker-compose'
-alias composer='docker-compose run --rm composer'
+alias composer='docker-compose run --rm --entrypoint composer php-cli'
+alias php='docker-compose run --rm php-cli'
+alias phpunit='docker-compose run --rm --entrypoint /project/bin/phpunit php-cli'
